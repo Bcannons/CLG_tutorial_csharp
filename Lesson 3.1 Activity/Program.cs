265f1aa201ad5ff8d391a387a34b67132f5f7013
Bcannons <95903491+Bcannons@users.noreply.github.com>
2022-01-10 15:21:41 +1100

    Activity 3.1
    Activity 3 named Activity 3.1 for Lesson 3 CLG

---
+++ Lesson 3.1 Activity/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("");
+
+Console.WriteLine("Welcome to your free Savings Calculator \n");
+
+Console.WriteLine("Please answer the follwing questions.\n");
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+
+Console.WriteLine("=====================================");
+
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+Console.WriteLine("Please enter your initial deposit \n");
+var initialDeposit = int.Parse(Console.ReadLine());
+Console.WriteLine($"You have entered {initialDeposit}\n");
+
+Console.WriteLine("Please enter your monthly interest rate % \n");
+var interestRate = double.Parse(Console.ReadLine());
+Console.WriteLine($"You have entered {interestRate}\n");
+
+Console.WriteLine("Please enter the number of months you will save for \n");
+var numberofMonths = int.Parse(Console.ReadLine());
+Console.WriteLine($"You have entered {numberofMonths}\n");
+
+Console.ResetColor();
+
+Console.WriteLine("Thank you, calculating total savings...\n");
+Console.WriteLine("=====================================\n");
+Console.WriteLine("");
+
+var savings = initialDeposit * Math.Pow((1 + interestRate),numberofMonths);
+Console.WriteLine($"Your total savings will be {savings}\n");
